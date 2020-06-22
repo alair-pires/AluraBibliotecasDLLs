1dc717d8006bb3e748ba22b842f9c77202492ae0
alair-pires <alair-pires@outlook.com>
2020-06-22 10:58:29 -0300

    Referencia de DLL

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,10 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            
+            ContaCorrente conta = new ContaCorrente(847, 615328);                        
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
         }
     }
 }
