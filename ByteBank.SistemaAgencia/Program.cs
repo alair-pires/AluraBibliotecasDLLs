2905451c7188cbfd5c158bb502fe7e37e75abfcc
alair-pires <alair-pires@outlook.com>
2020-06-22 13:20:17 -0300

    att

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+using Humanizer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,8 @@ namespace ByteBank.SistemaAgencia
             DateTime dataFimPagamento = new DateTime(2020, 7, 30);
             DateTime dataCorrente = DateTime.Now;
             TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
 
             Console.WriteLine(dataCorrente);
             Console.WriteLine(dataFimPagamento);
